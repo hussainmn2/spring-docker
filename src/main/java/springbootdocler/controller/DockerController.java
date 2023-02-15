c47eebeebff2223c076838045ba0c61b9a4233a5
HSNJAVA <94129228+HSNJAVA@users.noreply.github.com>
2023-02-15 23:01:14 +0530

    added the demo project to local repo

---
+++ src/main/java/springbootdocler/controller/DockerController.java
@@ -0,0 +1,17 @@
+package springbootdocler.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/getData")
+public class DockerController {
+
+	@GetMapping("/getDummy")
+	public String getDockerDummyMessage() {
+		return "## Welcome to Docker Application";
+
+	}
+
+}
